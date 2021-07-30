a6c4e4d4735c5192c0c860dbeecee2f4d1f2ad66
FoolyTD <trcariaga@utexas.edu>
2021-07-30 13:01:16 -0500

    added table, cards, buttons for play, and state

--- src/App.js
+++ src/App.js
@@ -1,75 +1,78 @@
+// importing use state to hold our vaiables critical to render, use effect to load state when page mounts
 import { useState, useEffect } from "react";
+// this function takes care of the work comparing the cards to score who won
+import scoreRound from "./scoreRound";
+// this is the style sheet
 import './App.css';
 
 function App() {
+	/* 
+	this deck variable is going to hold a string which is the deck id that returns from
+		our fetch call to our api "http://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1" 
+	*/
 	const [deck, setDeck] = useState([]);
-	const suits = ["spades", "diamonds", "clubs", "hearts"];
-	const values = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
+	// this will hold an object that contains four key/value pairs => // {value, image, suit, code}
+	const [playerCard, setPlayerCard] = useState([]);
+	// this will hold an object that contains four key/value pairs => // {value, image, suit, code}
+	const [opponentCard, setOpponentCard] = useState([]);
+	// boolean value to determine when cards have been played and are ready for scoring
+	const [roundOver, setRoundOver] = useState(false);
+	// this will hold the value of who has won the current round
+	const [score, setScore] = useState(null);
 	
-	const getDeck = () => {
-		const deck = [];
-	
-		for(let i = 0; i < suits.length; i++) {
-			for(let j = 0; j < values.length; j++) {
-				let card = {Value: values[j], Suit: suits[i]};
-				deck.push(card);
-			}
-		}
-		return deck;
-	}
-	
-	const shuffle = (deck) => {
-		// for 1000 turns
-		// switch the values of two random cards
-		for (let i = 0; i < 1000; i++) {
-			let location1 = Math.floor((Math.random() * deck.length));
-			let location2 = Math.floor((Math.random() * deck.length));
-			let tmp = deck[location1];
-	
-			deck[location1] = deck[location2];
-			deck[location2] = tmp;
-		}
-	}
-
+	// when the page mounts, make the fetch call and set the deck id
 	useEffect(()=> {
-		const newDeck = getDeck(); // newDeck = [{a2},{a3},{a4}]
-		shuffle(newDeck); // newDeck = [{a3},{a2},{a4}]
-		setDeck(newDeck);
+		fetch("http://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1")
+		.then((response)=>response.json())
+		.then((payload)=>setDeck(payload.deck_id))
 	}, [])
 	
-	// function renderDeck(deck) {
-	//       document.getElementById("deck").innerHTML = "";
-	
-	// 	for(let i = 0; i < deck.length; i++) {
-	// 		let card = document.createElement("div");
-	// 		let value = document.createElement("div");
-	// 		let suit = document.createElement("div");
-	// 		card.className = "card";
-	// 		value.className = "value";
-	// 		suit.className = "suit " + deck[i].Suit;
-	
-	// 		value.innerHTML = deck[i].Value;
-	// 		card.appendChild(value);
-	// 		card.appendChild(suit);
-	
-	// 		document.getElementById("deck").appendChild(card);
-	// 	}
-	// }
-	
-	// const deck = getDeck();
-	
-	function showDeck(deck) {
+	// this function wil run when the play card button is clicked
+	// it makes a fetch call to draw two cards from our deck at random
+	// then it sets one card as the players and the other as the opponents
+	// if also sets the round to over once cards have been set
+	const playCards = async () => {
+		setScore(null);
+		await fetch(`http://deckofcardsapi.com/api/deck/${deck}/draw/?count=2`)
+		.then((response)=>response.json())
+		.then((payload)=> {
+			setPlayerCard(payload.cards[0])
+			setOpponentCard(payload.cards[1])
+		})
+		.then(setRoundOver(true))		
 	}
-	console.log(deck);
+	
 
-
+	const resolveRound = () => {
+		setScore(scoreRound(playerCard.value, opponentCard.value))
+		setRoundOver(false);
+	}
 
   return (
     <div className="App">
-      <div className="deck">
-          <h1>A Deck of Cards</h1>
-		  <div></div>
-      </div>
+		<div className="title">
+          <h1 className="big-text">War</h1>
+		  <p></p>
+		</div>
+		  <div>
+			  <div className="table-container">
+				  <div className="table">
+						  <p className="card-text">Your Card</p>
+					  <div className="card">
+					  	<img src={roundOver && opponentCard.image}/>
+					  </div>
+						  <p className="card-text">Opponent Card</p>
+					  <div className="card">
+				  		<img src={roundOver && playerCard.image}/>
+					  </div>
+				  </div>
+			  </div>
+			  <div>
+				</div>
+				<div className="btn-container">
+			  <button className="btn" onClick={roundOver ? (resolveRound) : (playCards)}>{roundOver ? "Score" : "Play Card"}</button>
+				</div>
+		  </div>
     </div>
   );
 }
