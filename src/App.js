31321855259ad06250e781fdcb972449ae28e4a7
Priscilla <priscillakung@gmail.com>
2021-08-02 10:31:00 -0700

    implemented the sign in and sign out feature

--- src/App.js
+++ src/App.js
@@ -3,43 +3,43 @@ import { useState, useEffect } from "react";
 // Router will allow us to have multiple navigation pages
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 // Home component contains the game screen
-import Home from "./home/home";
+import Home from "./home/Home";
 // Login component has code for login screen
-import Login from "./login/login";
+import SignIn from "./components/SignIn";
 // this is the style sheet
 import "./App.css";
 import { auth } from "./firebase";
 
 function App() {
-// To keep track of the login status of the user, we need to use the onAuthStateChange to track the login status
-	const [user, setUser] = useState(null);
+  // To keep track of the login status of the user, we need to use the onAuthStateChange to track the login status
+  const [user, setUser] = useState(null);
 
-	useEffect(() => {
-		const unsubscribe = auth.onAuthStateChanged(userAuth => {
-			const user = {
-				uid: userAuth?.uid,
-				email: userAuth?.email
-			}
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged((userAuth) => {
+      const user = {
+        uid: userAuth?.uid,
+        email: userAuth?.email,
+      };
 
-			if (userAuth) {
-				console.log('userAuth', userAuth)
-				setUser(user);
-			} else {
-				setUser(null);
-			}
-		}) 
-		return unsubscribe;
-	}, []);
+      if (userAuth) {
+        console.log("userAuth", userAuth);
+        setUser(user);
+      } else {
+        setUser(null);
+      }
+    });
+    return unsubscribe;
+  }, []);
 
   return (
     <Router>
       <Switch>
         <Route exact={true} path="/">
-		{!user ? <Login /> : <Home />}
+          {!user ? <SignIn /> : <Home />}
         </Route>
-		<Route exact={true} path="/home">
-			<Home />
-		</Route>
+        <Route exact={true} path="/home">
+          <Home />
+        </Route>
       </Switch>
     </Router>
   );
